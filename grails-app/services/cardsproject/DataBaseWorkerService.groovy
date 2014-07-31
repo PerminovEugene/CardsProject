4b1e5b4d522e399e28ad503c75137591eb565634
Pavel Baybara <baybara.pavel@gmail.com>
2014-07-31 20:47:31 +0700

    Resolve conflicts

--- grails-app/services/cardsproject/DataBaseWorkerService.groovy
+++ grails-app/services/cardsproject/DataBaseWorkerService.groovy
@@ -5,6 +5,13 @@ import grails.transaction.Transactional
 @Transactional
 class DataBaseWorkerService {
 
+    def fixtureLoader
+
+    def startService()
+    {
+        fixtureLoader.load("dbFixtures")
+    }
+
     def serviceMethod() {
 
     }
