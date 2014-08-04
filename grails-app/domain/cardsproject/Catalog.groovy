5211383652f7c4a1e71340e80fb6f627b9f72d14
PerminovEugene <perminov.eugene@gmail.com>
2014-08-04 16:50:54 +0700

    database reworked

--- grails-app/domain/cardsproject/Catalog.groovy
+++ grails-app/domain/cardsproject/Catalog.groovy
@@ -1,10 +1,9 @@
 package cardsproject
 
-
-
 class Catalog {
     static hasMany = [picture: Picture]
     String name
+
     static constraints = {
         name(blank: true, size: 3..25, unique: true)
     }
