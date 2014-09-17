1d461cc3fda5f909b932f3367ad45bd1fc47968b
PerminovEugene <viewsonic_1993@mail.ru>
2014-09-17 17:03:05 +0700

    Redeploy production script is fixed

--- redeploy_production.sh
+++ redeploy_production.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-ssh bestrecards@178.62.0.194 -A '
-  cd /home/bestrecards/www/CardsProject/
-  git pull origin master
-  source ~/.gvm/bin/gvm-init.sh
-  gvm use grails 2.4.2
-  rm /home/bestrecards/www/CardsProject/grails-app/assets/pipeline -r
-  mkdir /home/bestrecards/www/CardsProject/grails-app/assets/pipeline
-  grails clean all
-  grails prod war target/ROOT.war
-'
+grails prod war target/ROOT.war
+scp ./target/ROOT.war root@178.62.0.194:/var/lib/tomcat7/webapps
 ssh root@178.62.0.194 -A '
-  cp /home/bestrecards/www/CardsProject/target/ROOT.war /var/lib/tomcat7/webapps
+   cd /home/bestrecards/www/CardsProject/
+   git pull origin master
   /etc/init.d/tomcat7 restart
 '
