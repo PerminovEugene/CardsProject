4b7571908f6545b19f01a3d3ffd80d7df6306234
PerminovEugene <viewsonic_1993@mail.ru>
2014-09-19 22:17:23 +0700

    Pictures reworked.

--- fixtures/dbFixtures.groovy
+++ fixtures/dbFixtures.groovy
@@ -44,56 +44,56 @@ holidays.save()
 
 def evaCard1 = new Picture()
 evaCard1.name = ("crazy man")
-evaCard1.path = 'assets/testCards/1.jpg'
+evaCard1.path = 'assets/testCards/card1.jpg'
 evaCard1.catalog = happyBday
 evaCard1.save()
 
 def evaCard2 = new Picture()
 evaCard2.name = ("happy girl")
-evaCard2.path = 'assets/testCards/2.jpg'
+evaCard2.path = 'assets/testCards/card2.jpg'
 evaCard2.catalog = valentinesDay
 evaCard2.save()
 
 def evaCard3 = new Picture()
 evaCard3.name = ("bubble goom girl")
-evaCard3.path = 'assets/testCards/3.jpg'
+evaCard3.path = 'assets/testCards/card3.jpg'
 evaCard3.catalog = holidays
 evaCard3.save()
 
 def picture = new Picture()
 picture.name = 'pict4'
-picture.path = 'assets/testCards/4.jpg'
+picture.path = 'assets/testCards/card4.jpg'
 picture.catalog = catalog
 picture.save()
 
 def picture1 = new Picture()
 picture1.name = 'pict5'
-picture1.path = 'assets/testCards/5.jpg'
+picture1.path = 'assets/testCards/card5.jpg'
 picture1.catalog = catalog
 picture1.save()
 
 def picture2 = new Picture()
 picture2.name = 'pict6'
 picture2.catalog = catalog
-picture2.path = 'assets/testCards/6.jpg'
+picture2.path = 'assets/testCards/card6.jpg'
 picture2.save()
 
 def picture3 = new Picture()
 picture3.name = 'pict7'
-picture3.path = 'assets/testCards/7.jpg'
+picture3.path = 'assets/testCards/card7.jpg'
 picture3.catalog = catalog
 picture3.save()
 
 def picture4 = new Picture()
 picture4.name = 'pict8'
-picture4.path = 'assets/testCards/8.jpg'
+picture4.path = 'assets/testCards/card8.png'
 picture4.catalog = catalog
 picture4.save()
 
 def picture5 = new Picture()
 picture5.name = 'pict9'
 picture5.catalog = catalog
-picture5.path = 'assets/testCards/9.jpg'
+picture5.path = 'assets/testCards/card9.png'
 picture5.save()
 
 
