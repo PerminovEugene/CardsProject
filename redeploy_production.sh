597342aafc91d0865f8d87169e8910ffa843f31a
PerminovEugene <viewsonic_1993@mail.ru>
2014-09-29 19:43:57 +0700

    Removed from redeploy work with git

--- redeploy_production.sh
+++ redeploy_production.sh
@@ -3,7 +3,5 @@
 grails prod war target/ROOT.war
 scp ./target/ROOT.war root@178.62.0.194:/var/lib/tomcat7/webapps
 ssh root@178.62.0.194 -A '
-   cd /home/bestrecards/www/CardsProject/
-   git pull origin master
   /etc/init.d/tomcat7 restart
 '
